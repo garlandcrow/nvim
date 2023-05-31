bbad25057d40eb587cd9835c29e1161448dd7aac
garlandcrow <garlandcrow@proton.me>
2023-05-31 17:51:42 +0900

    add back tailwind prettier typescript settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,9 @@
 return {
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  -- { import = "lazyvim.plugins.extras.linting.eslint" },
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
