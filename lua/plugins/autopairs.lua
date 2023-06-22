2faeef76b9851ddc2314f9112c8b12a05655358b
garlandcrow <garlandcrow@proton.me>
2023-06-23 08:19:44 +0900

    change autopairs plugin

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,8 @@
+return {
+  { "echasnovski/mini.pairs", enabled = false },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {}, -- this is equalent to setup({}) function
+  },
+}
